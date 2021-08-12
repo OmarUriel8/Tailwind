2e0ee80db722310aaec78a9b90c50b5a32ab02c3
OmarUriel8 <omarcarp08@gmail.com>
2021-08-12 17:50:50 -0500

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
+
+// npx tailwindcss build -i css/style.css -o css/app.css
